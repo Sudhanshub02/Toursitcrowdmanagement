211adb831e987d24867319c17c4b6d3031f4627c
riii112 <riyamehra2930@gmail.com>
2025-06-20 21:37:28 +0530

    Update graph.h

--- cpp/graph.h
+++ cpp/graph.h
@@ -1,4 +1,4 @@
-// Graph.h
+// graph.h
 #ifndef GRAPH_H
 #define GRAPH_H
 
@@ -8,16 +8,24 @@
 #include <set>
 #include <climits>
 #include <string>
+#include <vector>
+#include <algorithm>
+
 using namespace std;
 
 class Graph {
 private:
     unordered_map<string, list<pair<string, int>>> adj;
+    unordered_map<string, vector<string>> parent;
+    set<pair<string, string>> blockedEdges;
+
+    void printAllPaths(string src, string dst, vector<string> &path);
 
 public:
     void addEdge(string u, string v, int w, bool bidir = true);
+    void blockRoad(string u, string v);
     void printAdjList();
-    void dijkstra(string src);
+    void dijkstraAllPaths(string src, string dst);
 };
 
-#endif
+#endif // GRAPH_H
